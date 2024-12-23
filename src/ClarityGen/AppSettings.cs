5df6fafe36f94abc99af677de2ba480215c44996
sercanio <104576153+sercanio@users.noreply.github.com>
2024-12-23 22:45:38 +0300

    Add project files.

---
+++ src/ClarityGen/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace Myrtus.Clarity.Generator
+{
+
+    public class AppSettings
+    {
+        public TemplateSettings Template { get; set; }
+    }
+
+    public class TemplateSettings
+    {
+        public string GitRepoUrl { get; set; }
+        public string TemplateName { get; set; }
+    }
+}
