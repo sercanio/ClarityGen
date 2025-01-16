07f1595bd3c505ab533218d377ea0867ed7e5a7f
sercanio <104576153+sercanio@users.noreply.github.com>
2025-01-16 13:42:31 +0300

    fix!: fix configuration service

--- src/Myrtus.Clarity.Generator.DataAccess/ConfigurationService.cs
+++ src/Myrtus.Clarity.Generator.DataAccess/ConfigurationService.cs
@@ -9,20 +9,7 @@ namespace Myrtus.Clarity.Generator.DataAccess
     {
         public async Task<AppSettings?> LoadConfigurationAsync()
         {
-<<<<<<< HEAD
-            AnsiConsole.MarkupLine("[red]Error:[/] Configuration file not found");
-            return null;
-        }
-
-        try
-        {
-            var configContent = await File.ReadAllTextAsync(configPath);
-            var config = JsonSerializer.Deserialize<AppSettings>(configContent);
-
-            if (string.IsNullOrEmpty(config?.Template?.GitRepoUrl))
-=======
             try
->>>>>>> e93e48f (feat: embed appsettings.json file)
             {
                 var assembly = Assembly.GetExecutingAssembly();
                 var resourceName = "Myrtus.Clarity.Generator.DataAccess.appsettings.json"; // Ensure this matches the actual resource name
@@ -55,17 +42,6 @@ namespace Myrtus.Clarity.Generator.DataAccess
                 AnsiConsole.MarkupLine($"[red]Configuration Error:[/] {safeMessage}");
                 return null;
             }
-<<<<<<< HEAD
-
-            return config;
-        }
-        catch (Exception ex)
-        {
-            var safeMessage = Markup.Escape(ex.Message);
-            AnsiConsole.MarkupLine($"[red]Configuration Error:[/] {safeMessage}");
-            return null;
-=======
->>>>>>> e93e48f (feat: embed appsettings.json file)
         }
     }
 }
